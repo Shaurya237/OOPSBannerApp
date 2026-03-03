a84b4c19aacf3aca5d726e1c9c26b74249882038
Shaurya Pratap Singh <shauryapratapsingh@Shauryas-MacBook-Air-4.local>
2026-03-03 10:27:25 +0530

    UC6 - Refactored using static helper methods for O, P, S

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,33 +1,63 @@
-public class OOPSBANNERAPP {
+package com.oopsbanner;
 
-public static void main(String args[]) {
+public class OOPSBannerApp6 {
 
-
-
-    String[] lines = {
-
-        String.join("", " ** ", " ** ", " ** ", " ** "),
-
-        String.join("", "*   *", "*   *", "*   *", "**      "),
-
-        String.join("", "*   *", "*   *", "*   *", "**      "),
-
-        String.join("", "*   *", "*   *", "** ", " ***  "),
-
-        String.join("", "*   *", "*   *", "*      ", "     *"),   
-        String.join("", "*   *", "*   *", "*      ", "     *"),   
-        String.join("", " ** ", " ** ", "*      ", " ** ")
-
-    };
-
-
-
-    for (String line : lines) {
-
-        System.out.println(line);
-
+    // Helper Method for Letter O
+    static String[] getO() {
+        return new String[]{
+                " *** ",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*   *",
+                " *** "
+        };
     }
 
-}
+    // Helper Method for Letter P
+    static String[] getP() {
+        return new String[]{
+                "** ",
+                "*   *",
+                "*   *",
+                "** ",
+                "**     ",
+                "**     ",
+                "**     "
+        };
+    }
 
+    // Helper Method for Letter S
+    static String[] getS() {
+        return new String[]{
+                " *** ",
+                "**     ",
+                "**     ",
+                " *** ",
+                "     **",
+                "     **",
+                " *** "
+        };
+    }
+
+    public static void main(String[] args) {
+
+        String[] O1 = getO();
+        String[] O2 = getO();
+        String[] P = getP();
+        String[] S = getS();
+
+        // Build banner lines dynamically
+        String[] banner = new String[7];
+
+        for (int i = 0; i < 7; i++) {
+            banner[i] = O1[i] + " " + O2[i] + " " + P[i] + " " + S[i];
+        }
+
+        // Display banner
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
 }
