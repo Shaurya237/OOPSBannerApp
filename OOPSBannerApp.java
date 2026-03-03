c93f30a70fa4bc8de96450e92e95c722fffac209
Shaurya Pratap Singh <shauryapratapsingh@Shauryas-MacBook-Air-4.local>
2026-03-03 10:05:21 +0530

    UC4 - Implemented Arrays and Enhanced For Loop

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,33 +1,20 @@
-public class OOPSBannerApp {
+public class OOPSBannerApp 
+{
 
-    public static void main(String[] args) {
-        printBanner();
-    }
+    public static void main(String[] args) 
+    {
+        String[] bannerLines = new String[7];
+        bannerLines[0] = String.join("","  ****  ","  ****  "," ******  "," ****** ");
+        bannerLines[1] = String.join(""," **  ** "," **  ** "," **   ** ","**      ");
+        bannerLines[2] = String.join(""," **  ** "," **  ** "," **   ** ","**      ");
+        bannerLines[3] = String.join(""," **  ** "," **  ** "," ******  "," ****** ");
+        bannerLines[4] = String.join(""," **  ** "," **  ** "," **      ","      **");
+        bannerLines[5] = String.join(""," **  ** "," **  ** "," **      ","      **");
+        bannerLines[6] = String.join("","  ****  ","  ****  "," **      "," ****** ");
 
-    public static void printBanner() {
-
-        String line1 = String.join(" ",
-                "***", "***", "****", "****");
-
-        String line2 = String.join(" ",
-                "*   *", "*   *", "*   *", "*");
-
-        String line3 = String.join(" ",
-                "*   *", "*   *", "****", "***");
-
-        String line4 = String.join(" ",
-                "*   *", "*   *", "*", "   *");
-
-        String line5 = String.join(" ",
-                "***", "***", "*", "****");
-
-        System.out.println("==================================");
-        System.out.println(line1);
-        System.out.println(line2);
-        System.out.println(line3);
-        System.out.println(line4);
-        System.out.println(line5);
-        System.out.println("==================================");
-        System.out.println("Welcome to OOPS App");
+        for (String line : bannerLines) 
+        {
+            System.out.println(line);
+        }
     }
 }
