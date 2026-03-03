1b7aa1daf03a2e4acda446ef680f9abd93a7040a
Shaurya Pratap Singh <shauryapratapsingh@Shauryas-MacBook-Air-4.local>
2026-03-03 08:48:37 +0530

    Initial commit - OOPS Banner App

---
+++ OOPSBannerApp.java
@@ -0,0 +1,19 @@
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        printBanner();
+    }
+
+    public static void printBanner() {
+        System.out.println("==============================");
+        System.out.println("  OOO   OOO   PPPP   SSSS");
+        System.out.println(" O   O O   O  P   P S");
+        System.out.println(" O   O O   O  PPPP   SSS");
+        System.out.println(" O   O O   O  P         S");
+        System.out.println("  OOO   OOO   P     SSSS");
+        System.out.println("==============================");
+        System.out.println("Welcome to OOPS App");
+    }
+}
+
